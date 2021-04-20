6144aebd34d9f6314dd338922bb45cdeb038caeb
Vita Khutorianska <steblvita@gmail.com>
2021-04-20 23:26:53 +0300

    add lazy

--- src/App.js
+++ src/App.js
@@ -1,5 +1,9 @@
 // import './App.css';
 import { lazy, Suspense } from 'react-router-dom';
+import { NavLink, Route, Redirect, Switch } from 'react-router-dom';
+import styles from './App.module.css';
+const MoviesPage = lazy(() => import('./pages/MoviesPage'));
+const MovieDetailsPage = lazy(() => import('./pages/MovieDetailsPage '));
 
 function App() {
   return <div className="App"></div>;
